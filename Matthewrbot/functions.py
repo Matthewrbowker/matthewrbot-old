c53d411e59dddd5f8046247bd05a735996f74c5e
Matthew Bowker <matthewrbowker@me.com>
2013-12-17 15:24:19 -0700

    Remove old Python version

--- Matthewrbot/functions.py
+++
@@ -1,7 +0,0 @@
-#def sort(data):
-#    lines = data.split("\n");
-#    lines = [line for line in data if line.strip()];
-#    lines = lines.sort();
-#    return lines;
-#    return "\n" . join(lines);
-
